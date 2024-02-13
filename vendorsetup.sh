fc4fa892c45526310e3090fc4544f794dc2f8723
CuriousNom <aryanbiswas7585@gmail.com>
2024-02-13 08:35:32 +0530

    gta4lwifi: add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+# Cloning vendor repositories
+git clone --depth=1 https://github.com/CuriousNom/proprius_vendor_samsung_gta4lwifi.git vendor/samsung/gta4lwifi
+git clone --depth=1 https://github.com/CuriousNom/proprius_vendor_samsung_gta4l-common.git vendor/samsung/gta4l-common
+
+# Cloning necessary repository
+git clone --depth=1 https://github.com/CuriousNom/proprius_device_samsung_gta4l-common.git device/samsung/gta4l-common
+rm -rf hardware/samsung
+git clone --depth=1 https://github.com/LineageOS/android_hardware_samsung.git -b lineage-21 hardware/samsung
+
+# Cloning kernel repositories
+git clone --depth=1 https://github.com/CuriousNom/proprius_kernel_samsung_sm6115.git kernel/samsung/sm6115
