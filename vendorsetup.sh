6d4f9eb03bab457b851f749c0d49f874fe37f273
CuriousNom <aryanbiswas7585@gmail.com>
2024-02-24 01:31:54 -0500

    gta4lwifi: track repos from matrixx org

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,15 +1,15 @@
 # Cloning vendor repositories
-git clone --depth=1 https://github.com/CuriousNom/proprius_vendor_samsung_gta4lwifi.git vendor/samsung/gta4lwifi
-git clone --depth=1 https://github.com/CuriousNom/proprius_vendor_samsung_gta4l-common.git vendor/samsung/gta4l-common
+git clone --depth=1 https://github.com/Matrixx-Devices/android_vendor_samsung_gta4lwifi vendor/samsung/gta4lwifi
+git clone --depth=1 https://github.com/Matrixx-Devices/android_vendor_samsung_gta4l-common vendor/samsung/gta4l-common
 
 # Cloning necessary repository
-git clone --depth=1 https://github.com/CuriousNom/proprius_device_samsung_gta4l-common.git device/samsung/gta4l-common
-git clone --depth=1 https://github.com/LineageOS/android_hardware_samsung.git hardware/tmp
+git clone --depth=1 https://github.com/Matrixx-Devices/android_device_samsung_gta4l-common device/samsung/gta4l-common
+git clone --depth=1 https://github.com/Matrixx-Devices/android_hardware_samsung hardware/tmp
 cp -r hardware/tmp/* hardware/samsung/
 rm -rf hardware/tmp
 
 # Cloning kernel repositories
-git clone --depth=1 https://github.com/CuriousNom/kernel_samsung_gta4l.git -b 14.0-ksu kernel/samsung/sm6115
+git clone --depth=1 https://github.com/Matrixx-Devices/android_kernel_samsung_gta4l kernel/samsung/sm6115
 
 # Update KSU Targets
 cd kernel/samsung/sm6115
